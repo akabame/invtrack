5d0a4e2018bef59412972b8d00cae426053b9d4e
Simoes <lucas.simoes@solecapital.com.br>
2025-07-07 18:07:28 -0300

    feature: organize raw fund quota files before transform

---
+++ etl/standardize_fund_quota.py
@@ -0,0 +1,50 @@
+import os
+import pandas as pd
+from tqdm import tqdm
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parent.parent
+RAW_DATA_PATH = ROOT_DIR / "data" / "raw"
+PROCESSED_DATA_PATH = ROOT_DIR / "data" / "processed"
+
+class QuotaConsolidator():
+    
+    def __init__(self):
+        pass
+
+    def read_quota_history(self):
+        self.quota_dfs = {}
+        
+        # Loop through files in the directory
+        for filename in tqdm(os.listdir(RAW_DATA_PATH),'reading historic quota files'):
+            if filename.endswith('.xlsx'):
+                filepath = os.path.join(RAW_DATA_PATH, filename)
+                df = pd.read_excel(filepath)
+                key = os.path.splitext(filename)[0]  # filename without extension
+                self.quota_dfs[key] = df
+    
+    def group_files_by_fund_name(self):
+        self.quota_consolidated = {}
+        
+        for quota_key in tqdm(self.quota_dfs,'consolidating quota history'):
+            fund_name = quota_key.split('_')[0]
+            
+            if fund_name not in self.quota_consolidated:
+                self.quota_consolidated[fund_name] = pd.DataFrame()
+            
+            self.quota_consolidated[fund_name] = pd.concat([self.quota_consolidated[fund_name],self.quota_dfs[quota_key]])
+
+    def export_consolidated_quota(self):
+        for fund_name in self.quota_consolidated:
+            self.quota_consolidated[fund_name] = self.quota_consolidated[fund_name].sort_values('date')
+            self.quota_consolidated[fund_name].to_excel(rf"{PROCESSED_DATA_PATH}\{fund_name}_consolidated_quota.xlsx",index=False)
+
+    def run(self):
+        self.read_quota_history()
+        self.group_files_by_fund_name()
+        self.export_consolidated_quota()
+        self.export_consolidated_quota()
+
+if __name__ == '__main__':
+    temp = QuotaConsolidator()
+    temp.run()
